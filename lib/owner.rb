a44a6ba96ceb2c9001d87425f7f62ae8a65c2976
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-06 21:11:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,13 @@
 class Owner
   attr_accessor :pets, :name
   @@all = []
+  @count = 0
 
   def initialize(name)
     @name = name
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
+    @count += 1
   end
 
   def self.all
